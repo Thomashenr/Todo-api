7dd15fdae5c46d4b7ebbdd27ffed3be1b334ac31
Thomas Baker <ThomasHenr3@gmail.com>
2016-03-04 16:19:15 -0600

    hey coop

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
-	res.send('Todo API Root');
+	res.send('Todo API');
 });
 
 app.listen(PORT, function () {
