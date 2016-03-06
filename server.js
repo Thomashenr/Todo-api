c628269448d91165b77a0b5e67a0ed09079cae63
Thomas Baker <ThomasHenr3@gmail.com>
2016-03-06 13:59:27 -0600

    add /todos and /todos/:id

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ var todos = [{
 	id: 2,
 	description: 'Go to market',
 	completed: false
+}, {
+	id: 3,
+	description: 'Feed the cat',
+	completed: true
 }];
 
 app.get('/', function (req, res) {
@@ -16,7 +20,25 @@ app.get('/', function (req, res) {
 });
 
 app.get('/todos', function (req, res) {
-	response.json(todos);
+	res.json(todos);
+});
+
+app.get('/todos/:id', function (req, res) {
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo;
+
+	todos.forEach(function (todo) {
+		if (todoId === todo.id) {
+			matchedTodo = todo;
+		}
+	});
+
+	if (matchedTodo) {
+		res.json(matchedTodo);
+	} else {
+		res.status(404).send();
+	}
+
 });
 
 app.listen(PORT, function () {
